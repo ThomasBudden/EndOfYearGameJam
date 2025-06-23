eebcbe6cbb29d61f44ab02556c2a961c9cadd12e
ThomasBudden <buddenthomas04@gmail.com>
2025-06-23 15:44:00 +0100

    Made scripts

---
+++ EndOfYearGameJam/Assets/Scripts/ValueManagerScript.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class ValueManagerScript : MonoBehaviour
+{
+    public float f; //f = Influence / dark energy - currency allows you to unlock and upgrade cards, allows you to get win condition after accumulating a certain amount.
+    public float i; //i = infamy - negative multiplier that affects influence
+    public GameObject indocManager;
+    public GameObject leafletManager;
+    public GameObject kidnapManager;
+    public GameObject socMedManager;
+    public GameObject mindConManager;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
